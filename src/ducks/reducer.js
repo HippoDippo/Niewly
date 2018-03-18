6c3d5898562d832bfe2224b983dbaa8c502a289a
Kaycee Ingram <KayceeIngram33@gmail.com>
2018-03-17 20:23:30 -0600

    Added Redux

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,10 +1,25 @@
-// Initial State.
-const initialState = {
-  //
+// Initial State
+const initialState = { postID: 0 };
+
+// Actions
+const UPDATE_POST_ID = 'UPDATE_POST_ID';
+
+// Action Creators
+export function updatePostId(postID) {
+  return {
+    type: UPDATE_POST_ID,
+    payload: postID
+  }
 }
 
-// Actions.
-
-// Action Creators.
-
-// Reducer.
+// Reducer
+export default function reducer(state=initialState, action) {
+  switch (action.type) {
+    case UPDATE_POST_ID:
+      return {
+        postID: action.payload
+      };
+    default:
+      return state;
+  }
+}
