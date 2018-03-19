00def650eb6c1abad54b623d57658f3cc9525bc4
Kaycee Ingram <KayceeIngram33@gmail.com>
2018-03-19 17:12:33 -0600

    improved formatting

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -45,28 +45,19 @@ class Posts extends React.Component {
   render() {
 
     let userPosts = this.state.posts.map((e, i, arr) => {
-      return (
-      //        <div key={arr[i].id} id={arr[i].id} className="post">
-      //           <h3 className="post-title">{arr[i].post_title}</h3>
-      //           <h4 className="post-intro">{arr[i].post_intro}</h4>
-      //           <p className="author">{arr[i].post_author}</p>
-      //           {/* <button onClick={this.handleClickSave.bind(this, arr[i].id)} className="save-button">Save</button> */}
-      //           <Link to="/postView" onClick={this.handleClickView.bind(this, arr[i].id)} className="view-button">View</Link>
-      //         </div>
-
-        <div key={arr[i].id} id={arr[i].id} className="Post">
-            <div className="Post-header">
-            <h2 className="Post-heading">{arr[i].post_title}</h2>
-          </div>
-          <div className="Post-content">
-            <ul className="Post-items">
-              <li className="Post-item">{arr[i].post_intro}</li>
-              <li className="Post-item">Author: <span className="author">{arr[i].post_author}</span></li>
-              <Link to="/postView" onClick={this.handleClickView.bind(this, arr[i].id)} className="Post-item view-button"><li>View</li></Link>
-              <li onClick={this.handleClickSave.bind(this, arr[i].id)} className="Post-item save-button">Save</li>
-            </ul>
-          </div>
-        </div>
+      return (<div key={arr[i].id} id={arr[i].id} className="Post">
+                <div className="Post-header">
+                  <h2 className="Post-heading">{arr[i].post_title}</h2>
+                </div>
+                <div className="Post-content">
+                  <ul className="Post-items">
+                    <li className="Post-item">{arr[i].post_intro}</li>
+                    <li className="Post-item">Author: <span className="author">{arr[i].post_author}</span></li>
+                    <Link to="/postView" onClick={this.handleClickView.bind(this, arr[i].id)} className="Post-item view-button"><li>View</li></Link>
+                    <li onClick={this.handleClickSave.bind(this, arr[i].id)} className="Post-item save-button">Save</li>
+                  </ul>
+                </div>
+              </div>
       );
     });
 
