7e1228fed9c240cc38fab249fd58220ffa8511aa
Kaycee Ingram <KayceeIngram33@gmail.com>
2018-03-20 14:40:32 -0600

    editor almost done

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,17 +1,57 @@
 import React from 'react';
 import './Editor.css';
+import axios from 'axios';
 
-export default function Editor(props) {
-  return (
-    <div className="editor">
-      <div>
-        <textarea className="input"/>
+class Editor extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      editorInput: '',
+      post: ''
+    };
+
+    this.handleEditorInput = this.handleEditorInput.bind(this);
+    this.handleEditorClick = this.handleEditorClick.bind(this);
+  }
+
+  extractContents() {
+    //
+
+    return {
+      title: '',
+      intro: '',
+      body: ''
+    }
+  }
+
+  handleEditorInput(event) {
+    this.setState({
+      editorInput: event.target.value
+    });
+  }
+
+  handleEditorClick() {
+    this.setState({
+      post: extractContents(this.state.editorInput)
+    });
+    let { post } = this.state.post;
+    axios.post('/api/createPost', { title: post.title, intro: post.intro, etc })
+  }
+
+  render() {
+
+    return (
+      <div className="editor">
+        <div>
+          <textarea onChange={(e) => this.handleEditorInput(e)} className="input"/>
+        </div>
+        <div>
+          <button onClick={this.handleEditorClick} className="button">Post</button>
+        </div>
       </div>
-      <div>
-        <button className="button">Post</button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 // function extractID(str) {
