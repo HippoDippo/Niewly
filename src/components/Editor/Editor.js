7c9df5083648a366d308a7ae5bd616f7c648fdae
Kaycee Ingram <KayceeIngram33@gmail.com>
2018-03-21 19:54:51 -0600

    Editor almost finished

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -15,14 +15,65 @@ class Editor extends React.Component {
     this.handleEditorClick = this.handleEditorClick.bind(this);
   }
 
-  extractContents() {
-    //
+  getTitleIndex(text) {
+    var textCpy = text.split(' ');
+
+    textCpy.shift(); // Remove first #.
+    console.log(textCpy);
+    return {
+      start: 1,
+      end: textCpy.indexOf('#') + 1
+    };
+  }
+
+  getIntroIndex(text) {
+    var textCpy = text.split(' ')
+      , start
+      , end;
+
+    for (var y = 0; y < 2; y++) {
+      if (y === 0) {
+        start = textCpy.indexOf('##');
+        textCpy.splice(start, 1);
+      } else {
+        end = textCpy.indexOf('##');
+      }
+    }
 
     return {
-      title: '',
-      intro: '',
-      body: ''
-    }
+      start: start+1,
+      end: end+1
+    };
+  }
+
+  getBodyIndex(text) {
+    var textCpy = text.split(' ')
+      , start;
+
+    textCpy.splice(textCpy.indexOf('##'), 1); // Remove first ##.
+    start = textCpy.indexOf('##') + 2;
+
+    return {
+      start: start,
+      end: text.length - 1
+    };
+  }
+
+  extractContents(text) {
+    var titleIndexes = getTitleIndex(text)
+     , introIndexes = getIntroIndex(text)
+     , bodyIndexes = getBodyIndex(text)
+     , textCpy = text.split(' ');
+
+    var title = textCpy.slice(titleIndexes.start, titleIndexes.end)
+      , intro = textCpy.slice(introIndexes.start, introIndexes.end)
+      , body = textCpy.slice(bodyIndexes.start, bodyIndexes.end);
+
+    return {
+      title: title.join(' '),
+      intro: intro.join(' '),
+      body: body.join(' ')
+    };
   }
 
   handleEditorInput(event) {
@@ -36,7 +87,7 @@ class Editor extends React.Component {
       post: extractContents(this.state.editorInput)
     });
     let { post } = this.state.post;
-    axios.post('/api/createPost', { title: post.title, intro: post.intro, etc })
+    axios.post('/api/createPost', { title: post.title, intro: post.intro, body: post.body, author: post.author, etc })
   }
 
   render() {
