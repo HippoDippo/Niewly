52db4ea704218c179cdf241448163e492bf76111
Kaycee Ingram <KayceeIngram33@gmail.com>
2018-03-25 19:08:45 -0600

    working on bookmarks

--- src/components/BookMarks/BookMarks.js
+++ src/components/BookMarks/BookMarks.js
@@ -15,38 +15,38 @@ class BookMarks extends React.Component {
 
   componentDidMount() {
     axios.all([
-      axios.get('/api/getBookmarks/' + this.state.userID),
+      axios.get('/api/getBookmarks/' + this.state.userID), // userID does not exist yet (look on next line).
       axios.get('/auth/me')
     ])
     .then(axios.spread((bookmarkRes, authRes) => {
+      console.log(bookmarkRes);
       this.setState({
         bookmarks: [...bookmarkRes.data],
         userID: authRes.data.user_id
       });
     }));
-    console.log(this.state.bookmarks);
   }
 
   render() {
 
-    let userPosts = this.state.bookmarks.map((e, i, arr) => {
-      return (<div key={arr[i].id} id={arr[i].id} className="Post">
-                <div className="Post-header">
-                  <h2 className="Post-heading">{arr[i].post_title}</h2>
-                </div>
-                <div className="Post-content">
-                  <ul className="Post-items">
-                    <li className="Post-item Post-intro">{arr[i].post_intro}</li>
-                    <li className="Post-item">Author: <span className="author">{arr[i].post_author}</span></li>
-                    <div className="Post-buttons">
-                      <Link to="/postView" onClick={this.handleClickView.bind(this, arr[i].id)} className="Post-item view-button"><li>View</li></Link>
-                      <li onClick={this.handleClickSave.bind(this, arr[i].id)} className="Post-item save-button">Save</li>
-                    </div>
-                  </ul>
-                </div>
-              </div>
-      );
-    });
+    // let userPosts = this.state.bookmarks.map((e, i, arr) => {
+    //   return (<div key={arr[i].id} id={arr[i].id} className="Post">
+    //             <div className="Post-header">
+    //               <h2 className="Post-heading">{arr[i].post_title}</h2>
+    //             </div>
+    //             <div className="Post-content">
+    //               <ul className="Post-items">
+    //                 <li className="Post-item Post-intro">{arr[i].post_intro}</li>
+    //                 <li className="Post-item">Author: <span className="author">{arr[i].post_author}</span></li>
+    //                 <div className="Post-buttons">
+    //                   <Link to="/postView" onClick={this.handleClickView.bind(this, arr[i].id)} className="Post-item view-button"><li>View</li></Link>
+    //                   <li onClick={this.handleClickSave.bind(this, arr[i].id)} className="Post-item save-button">Save</li>
+    //                 </div>
+    //               </ul>
+    //             </div>
+    //           </div>
+    //   );
+    // });
 
     return (
       <div>
