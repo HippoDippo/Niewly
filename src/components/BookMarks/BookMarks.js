87d364abc48ff2b6eaab3766cd11a1230852f179
Kaycee Ingram <KayceeIngram33@gmail.com>
2018-03-27 14:19:23 -0600

    bookmarks almost done

--- src/components/BookMarks/BookMarks.js
+++ src/components/BookMarks/BookMarks.js
@@ -18,38 +18,48 @@ class BookMarks extends React.Component {
     .then(bookmarksRes => {
       axios.get('/api/getPost/' + bookmarksRes.data[0].post_id)
       .then(postsRes => {
-        console.log(postsRes);
         this.setState({
-          bookmarks: postsRes.data
+          bookmarks: [...postsRes.data]
         });
       });
     });
   }
 
+  handleClickDelete(i, event) {
+    let selectedPost = this.state.posts.filter(post => {
+      return post.id === i;
+    });
+    axios.delete('/api/deleteBookmark', {user_id: this.props.userID, post_id: i});
+  }
+
+  handleClickView(i, event) {
+    this.props.dispatch(updatePostId(i));
+  }
+
   render() {
 
-    // let userPosts = this.state.bookmarks.map((e, i, arr) => {
-    //   return (<div key={arr[i].id} id={arr[i].id} className="Post">
-    //             <div className="Post-header">
-    //               <h2 className="Post-heading">{arr[i].post_title}</h2>
-    //             </div>
-    //             <div className="Post-content">
-    //               <ul className="Post-items">
-    //                 <li className="Post-item Post-intro">{arr[i].post_intro}</li>
-    //                 <li className="Post-item">Author: <span className="author">{arr[i].post_author}</span></li>
-    //                 <div className="Post-buttons">
-    //                   <Link to="/postView" onClick={this.handleClickView.bind(this, arr[i].id)} className="Post-item view-button"><li>View</li></Link>
-    //                   <li onClick={this.handleClickSave.bind(this, arr[i].id)} className="Post-item save-button">Save</li>
-    //                 </div>
-    //               </ul>
-    //             </div>
-    //           </div>
-    //   );
-    // });
+    let userBookmarks = this.state.bookmarks.map((e, i, arr) => {
+      return (<div key={arr[i].id} id={arr[i].id} className="Post">
+                <div className="Post-header">
+                  <h2 className="Post-heading">{arr[i].post_title}</h2>
+                </div>
+                <div className="Post-content">
+                  <ul className="Post-items">
+                    <li className="Post-item Post-intro">{arr[i].post_intro}</li>
+                    <li className="Post-item">Author: <span className="author">{arr[i].post_author}</span></li>
+                    <div className="Post-buttons">
+                      <Link to="/postView" onClick={this.handleClickView.bind(this, arr[i].id)} className="Post-item view-button"><li>View</li></Link>
+                      <li onClick={this.handleClickDelete.bind(this, arr[i].id)} className="Post-item delete-button">Delete</li>
+                    </div>
+                  </ul>
+                </div>
+              </div>
+      );
+    });
 
     return (
-      <div>
-        <h1>Bookmarks</h1>
+      <div className="bookmarks">
+        {userBookmarks}
       </div>
     );
   }
@@ -57,7 +67,8 @@ class BookMarks extends React.Component {
 
 function mapStateToProps(state) {
   return {
-    userID: state.userID
+    userID: state.userID,
+    postID: state.postID
   }
 }
 
