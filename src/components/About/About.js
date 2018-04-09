3783ae3b7284b8adfb611ca1dcb7167caee8cb7f
Kaycee Ingram <KayceeIngram33@gmail.com>
2018-04-08 22:01:57 -0600

    polishing it up

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,6 +6,7 @@ export default function About(props) {
     <div className="about">
       <h1 className="about-heading">About Niewly</h1>
       <p className="about-body">Niewly is a place where people just starting out in the tech industry can share their experiences, findings, and general wisdom for a mutually beneficial cause. Long story short, Niewly is where Stack Overflow meets Medium.</p>
+      <p>The awesome magnifying glass icon on the users page was created by <a href="https://thenounproject.com/vijayashastry362/">Fantastic</a>.</p>
     </div>
   );
 }
