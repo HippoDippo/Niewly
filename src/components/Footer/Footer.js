5fa375d72ce4600a5324ecf4b376716ed9d547bb
Kaycee Ingram <KayceeIngram33@gmail.com>
2018-04-12 13:13:27 -0600

    changed link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 export default function Footer(props) {
   return (
     <div className="footer">
-      { props.user ? <h3 className="footer-message">{'<'} Coded with ❤️ by <a href="http://kaycees-dandy-project.webflow.io/">Kaycee Ingram</a> {'/>'} {'<'} <a href="http://localhost:3000/#/about">About</a> {'/>'}</h3> : <h3 className="footer-message">{'<'} Coded with ❤️ by <a href="http://www.HippoDippo.com">Kaycee Ingram</a> {'/>'}</h3> }
+      { props.user ? <h3 className="footer-message">{'<'} Coded with ❤️ by <a href="http://kaycees-dandy-project.webflow.io/">Kaycee Ingram</a> {'/>'} {'<'} <a href="http://localhost:3000/#/about">About</a> {'/>'}</h3> : <h3 className="footer-message">{'<'} Coded with ❤️ by <a href="http://kaycees-dandy-project.webflow.io/">Kaycee Ingram</a> {'/>'}</h3> }
     </div>
   );
 }
